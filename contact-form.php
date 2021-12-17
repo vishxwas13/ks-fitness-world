4600ba851d2ef758eaf94191f4c94a9e2a51a072
Vishwas Sharma <vishwas.sharma_cs19@gla.ac.in>
2021-12-17 16:23:21 +0530

    ks-fitness-world-first_commit

---
+++ contact-form.php
@@ -0,0 +1,18 @@
+<?php
+
+$username = "root";
+$password = "";
+$server = "localhost";
+$db = "ks-fitness-world";
+
+$con = mysqli_connect($server,$username,$password,$db);
+if($db){
+   // echo"connection successful";
+?>
+<script>
+    alert('connection successful vishxwas!');
+    </script>
+<?php
+}
+ else{ echo"connection failed"; }
+?>
